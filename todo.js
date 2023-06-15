774f516388bb55d1007d34e87092a2fa4bae8918
takehara naoya <takehara@throo.io>
2023-06-15 17:21:01 +0900

    add 修正課題81.js

--- todo.js
+++ todo.js
@@ -1,28 +1,19 @@
-// No.78 add start
-function adduser() {
-    const username = document.getElementById("username");
-    const adduser = document.createElement("div");
-    adduser.className = "todo-item";
-    adduser.innerText = username.value;
-    document.body.appendChild(adduser);
-// No.79 add start
-    username.value = '';
-// No.79 add end
-// No.80 add start
-    const classcount = document.getElementsByClassName("todo-item").length;
-    const classcountElement = document.getElementById("classcount");
-    classcountElement.innerHTML = '未完了のタスク:' + classcount;
-// No.80 add end
-// No.81.1 add end
-    adduser.setAttribute("onclick", "classChange(this)");
-// No.81.1 add start
+function addtodo() {
+    const insertname = document.getElementById("addname");
+    const todoname = document.createElement("div");
+    todoname.className = "todo-item";
+    todoname.innerText = insertname.value;
+    document.body.appendChild(todoname);
+    insertname.value = '';
+    const todocount = document.getElementsByClassName("todo-item").length;
+    const todolist = document.getElementById("No.todo");
+    todolist.innerHTML = '未完了のタスク:' + todocount;
+    todoname.setAttribute("onclick", "endtask(this)");
 }
-// No.78 add end
-// No.81.2 add start
-function classChange(a) {
+
+function endtask(a) {
     a.className = "completed-todo-item";
-    const classcount = document.getElementsByClassName("todo-item").length;
-    const classcountElement = document.getElementById("classcount");
-    classcountElement.innerHTML = '未完了のタスク:' + classcount;
+    const todocount = document.getElementsByClassName("todo-item").length;
+    const todolist = document.getElementById("No.todo");
+    todolist.innerHTML = '未完了のタスク:' + todocount;
 }
-// No.81.2 add end
