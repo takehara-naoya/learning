621f2d5976fd43b08d1c5157e5361c662d41d544
takehara naoya <takehara@throo.io>
2023-06-20 12:54:31 +0900

    add 課題83.js

--- todo.js
+++ todo.js
@@ -1,19 +1,25 @@
 function addtodo() {
-    const insertname = document.getElementById("addname");
-    const todoname = document.createElement("div");
-    todoname.className = "todo-item";
-    todoname.innerText = insertname.value;
-    document.body.appendChild(todoname);
-    insertname.value = '';
+    const todonameFiled = document.getElementById("todo-name-Filed");
+    const todoItem = document.createElement("div");
+    todoItem.className = "todo-item";
+    todoItem.innerText = todonameFiled.value;
+    document.body.appendChild(todoItem);
+    todonameFiled.value = '';
     const todocount = document.getElementsByClassName("todo-item").length;
     const todolist = document.getElementById("No.todo");
     todolist.innerHTML = '未完了のタスク:' + todocount;
-    todoname.setAttribute("onclick", "endtask(this)");
+    todoItem.setAttribute("onclick", "completedtask(this)");
 }
 
-function endtask(a) {
-    a.className = "completed-todo-item";
-    const todocount = document.getElementsByClassName("todo-item").length;
-    const todolist = document.getElementById("No.todo");
-    todolist.innerHTML = '未完了のタスク:' + todocount;
+//クラス名が"todo-item"なら"completed-todo-item"を入れ、"completed-todo-item"なら"todo-item"を入れる。
+function completedtask(a) { 
+   if (a.className == "todo-item") {
+        a.className = "completed-todo-item";
+   }
+   else {
+        a.className = "todo-item";
+   }
+   const todocount = document.getElementsByClassName("todo-item").length;
+   const todolist = document.getElementById("No.todo");
+   todolist.innerHTML = '未完了のタスク:' + todocount;
 }
