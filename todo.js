8aee799b11ec3765b4b31a350053e32563b83ef3
takehara naoya <takehara@throo.io>
2023-06-22 13:51:21 +0900

    課題84.js

--- todo.js
+++ todo.js
@@ -1,14 +1,24 @@
+window.onload = function () { //タブ起動時にlocalstrageを読み込み、データを"stock_todo"に挿入
+    savedtodo = localStorage.getItem("todo-key");
+    document.getElementById("stock_todo").innerHTML = savedtodo;
+    const todocount = document.getElementsByClassName("todo-item").length;
+    const todolist = document.getElementById("No.todo");
+    todolist.innerHTML = '未完了のタスク:' + todocount;
+}
+
 function addtodo() {
     const todonameFiled = document.getElementById("todo-name-Filed");
     const todoItem = document.createElement("div");
     todoItem.className = "todo-item";
     todoItem.innerText = todonameFiled.value;
-    document.body.appendChild(todoItem);
+    document.getElementById("stock_todo").appendChild(todoItem); //"stock_todo"に"todo-item"を追加
     todonameFiled.value = '';
     const todocount = document.getElementsByClassName("todo-item").length;
     const todolist = document.getElementById("No.todo");
     todolist.innerHTML = '未完了のタスク:' + todocount;
     todoItem.setAttribute("onclick", "completedtask(this)");
+    let savedtodo = document.getElementById("stock_todo").innerHTML; //"stock_todo"のHTML要素を取得し、savedtodoに挿入
+    localStorage.setItem("todo-key", savedtodo); //localstrageに保存
 }
 
 //クラス名が"todo-item"なら"completed-todo-item"を入れ、"completed-todo-item"なら"todo-item"を入れる。
